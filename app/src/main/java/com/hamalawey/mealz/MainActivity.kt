efa5dd950b6ad5d8d6db28a69206d858ecb6661b
heshamabdla <heshamabdla12@gmail.com>
2024-04-12 22:39:47 +0200

    Initial commit

---
+++ app/src/main/java/com/hamalawey/mealz/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.hamalawey.mealz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
+import com.hamalawey.mealz.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val mainVM: MealsViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mainVM.getMeals()
+        val mealsAdapter= MealsAdapter()
+
+        lifecycleScope.launch {
+            mainVM.categories.collect{
+                mealsAdapter.submitList(it?.categories)
+                binding.categoryRv.adapter=mealsAdapter
+            }
+        }
+
+
+    }
+}
